810594413f76d45cc33ff8b8705d10ef15805d60
Manikya-Sharma <92919520+Manikya-Sharma@users.noreply.github.com>
2024-11-17 18:09:45 +0530

    basic database + auth

--- src/lib/authUtils.ts
+++ src/lib/authUtils.ts
@@ -1,6 +1,8 @@
 "use server";
 
-import { signIn, signOut } from "@/auth";
+import { auth, signIn, signOut } from "@/auth";
+import type { User } from "@prisma/client";
+import { db } from "./db";
 
 export async function serverSignIn(provider: "github" | "google") {
   await signIn(provider, { redirectTo: "/dashboard" });
@@ -8,4 +10,19 @@ export async function serverSignIn(provider: "github" | "google") {
 
 export async function serverSignOut() {
   await signOut({ redirectTo: "/" });
+}
+
+export async function getUser(): Promise<User | null> {
+  const session = await auth();
+  if (!session || !session.user) {
+    return null;
+  }
+
+  const account = await db.user.findFirst({
+    where: {
+      externalId: session.user.id,
+    },
+  });
+
+  return account;
 }
